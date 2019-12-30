a9307cf156912db38b1e27d07c4a540b3cd79a2e
Jonathan <parrottjonathanc@gmail.com>
2019-12-30 16:26:22 -0700

    I4: Added color changing and guess limiter

--- web_guesser.rb
+++ web_guesser.rb
@@ -2,26 +2,36 @@ require 'sinatra'
 require 'sinatra/reloader'
 
 
-$NUMBER = rand(100)
-  
+@@number = rand(100)
+@@guesses_left = 5
 
 def check_guess(guesser)
   case
-  when $guess > $NUMBER + 5
-    "Way too high!" 
-  when $guess > $NUMBER 
-    "Too high!"
-  when $guess < $NUMBER - 5
-    "Way too low!"
-  when $guess < $NUMBER
-    "Too low!"
+  when $guess > @@number + 5
+    ["Way too high!", "red"]
+  when $guess > @@number 
+    ["Too high!", "pink"]
+  when $guess < @@number - 5
+    ["Way too low!", "red"]
+  when $guess < @@number
+    ["Too low!", "pink"]
   else
-    "You got it right! The SECRET NUMBER is #{$NUMBER} and "
+    ["You got it right! The SECRET NUMBER is #{@@number} and ", "green"]
+  end
+end
+
+def reset_checker()
+  if @@guesses_left == 0
+    @@guesses_left = 5
+    @@number = rand(100)
   end
 end
 
 get '/' do
   $guess = params['guess'].to_i
-  message = check_guess($NUMBER)
-  erb :index, :locals => {:number => $NUMBER, :message => message}
+  message = check_guess(@@number)[0]
+  color = check_guess(@@number)[1]
+  @@guesses_left -= 1
+  reset_checker()
+  erb :index, :locals => {:number => @@number, :message => message, :color => color, :guesses_left => @@guesses_left}
 end
